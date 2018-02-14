0f134bac08ba4561183e7c68893a7c7ee967b96d
ndkcha <ndkcha@gmail.com>
2018-02-14 11:33:12 -0500

    Unit testing

---
+++ src/app/chip/chip.component.spec.ts
@@ -0,0 +1,104 @@
+/**
+ * The test component for the ChipComponent
+ * @author ndkcha
+ */
+
+import { ChipComponent } from "./chip.component";
+import { ComponentFixture, TestBed, async, tick, fakeAsync, flushMicrotasks } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { Subscription } from "rxjs/Subscription";
+
+let compInst: ChipComponent;
+let fixture: ComponentFixture<ChipComponent>;
+
+describe('ChipComponent', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [FormsModule, ReactiveFormsModule],
+            declarations: [ChipComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(async(() => {
+        fixture = TestBed.createComponent(ChipComponent);
+        compInst = fixture.componentInstance;
+
+        compInst.chips = [];
+        compInst.options = [];
+        compInst.threshold = 2;
+        compInst.template = undefined;
+        compInst.placeholder = "Search here";
+
+        fixture.detectChanges();
+    }));
+
+    it("should create the component", () => {
+        expect(compInst).toBeTruthy();
+    });
+
+    it("should initialize the values properly", () => {
+        expect(compInst.chips.length).toBe(0, "chips");
+        expect(compInst.options.length).toBe(0, "options");
+        expect(compInst.threshold).toBe(2, "threshold");
+        expect(compInst.template).toBeUndefined("template");
+        expect(compInst.placeholder).toBe("Search here", "placeholder");
+    });
+
+    it("should call onQuery", () => {
+        let query: string = "search";
+
+        compInst.onQueryTriggered(query);
+        
+        let onQ: Subscription = compInst.onQuery.subscribe((value: string): void => {
+            expect(value).toBe(query);
+            onQ.unsubscribe();
+        });
+    });
+
+    it("should call onAdd", () => {
+        let input: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+        let i: string = "Anand";
+        compInst.options = ["Anand"];
+        
+        compInst.addItem(0, input);
+
+        expect(compInst.options.length).toBe(0, "options.length");
+        expect(compInst.chips.length).toBe(1, "chips.length");
+
+        let onA: Subscription = compInst.onAdd.subscribe((item: string): void => {
+            expect(item).toBe(i, "item");
+            onA.unsubscribe();
+        });
+    });
+
+    it("should call onDelete", () => {
+        let i: string = "Anand";
+        compInst.chips = ["Anand"];
+
+        compInst.deleteChip(0);
+
+        expect(compInst.chips.length).toBe(0, "chips.length");
+
+        let onD: Subscription = compInst.onRemove.subscribe((item: string): void => {
+            expect(item).toBe(i, "item");
+            onD.unsubscribe();
+        });
+    });
+
+    it("threshold should work as expected", fakeAsync(() => {
+        let query: string = "q";
+        let spy = spyOn(compInst.onQuery, "emit");
+
+        compInst.onQueryTriggered(query);
+        tick(1000);
+        
+        expect(spy.calls.count()).toBe(0, "with q");
+
+        query = "query";
+        compInst.onQueryTriggered(query);
+        tick(1000);
+
+        expect(spy.calls.count()).toBe(1, "with query");
+    }));
+});
